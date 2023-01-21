30f488568705e702bab7a26b078321d300c0f3d4
Daniel <danielrodrigues0709@gmail.com>
2023-01-21 13:35:06 -0300

    test: working on test classes

--- src/app/shared/services/products.service.spec.ts
+++ src/app/shared/services/products.service.spec.ts
@@ -1,16 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { inject, TestBed, waitForAsync } from '@angular/core/testing';
+import { toArray } from 'rxjs';
 
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ProductsService],
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ProductsService);
+    httpController = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return all products', inject([ProductsService], async(productsService: ProductsService) => {
+    let array = [];
+    await productsService.getProducts().subscribe(res => {
+      res.forEach((element: any) => {
+        array.push(element)
+      });
+      expect(array.length).toBeGreaterThan(700);
+    })
+    
+    
+    // const url = service.apiUrl;
+    // const http = httpController.expectOne(service.apiUrl)
+  }));
 });
