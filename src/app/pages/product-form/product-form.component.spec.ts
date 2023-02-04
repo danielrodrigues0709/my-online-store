ee4c88f0604c9f8eb62169f6e6a939dfd710cdcc
Daniel <danielrodrigues0709@gmail.com>
2023-02-04 03:25:00 -0300

    test: adding tests

--- src/app/pages/product-form/product-form.component.spec.ts
+++ src/app/pages/product-form/product-form.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 import { ProductFormComponent } from './product-form.component';
 
@@ -8,7 +12,11 @@ describe('ProductFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductFormComponent ]
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [ ProductFormComponent ],
+      providers: [DynamicDialogRef, DynamicDialogConfig, FormBuilder, MessageService]
     })
     .compileComponents();
 
@@ -17,7 +25,10 @@ describe('ProductFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should render form", () => {
+    const fixture = TestBed.createComponent(ProductFormComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('form').innerHTML).toBeTruthy();
   });
 });
